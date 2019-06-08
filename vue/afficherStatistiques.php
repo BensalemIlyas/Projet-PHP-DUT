5ae3a9d93e02e4f47eb8a8edf940236cd405be47
ilyas b <ilyashitchens@gmail.com>
2019-06-08 20:34:58 +0200

    eztgrgr

--- vue/afficherStatistiques.php
+++ vue/afficherStatistiques.php
@@ -2,6 +2,13 @@
 require_once 'authenfication.php';
 require_once 'Menu.php';
 require_once '../controleur/Consultation.php';
+?>
+</br></br></br>
+<H1> Statistiques des patients ayant déjà pris un rendez vous avec un medecin </H1>
+<?php
 echo Consultation::afficherStatistiquesUsagers();
+?>
+<H1> Statistiques relatives aux consultations pour chaque medecin </H1>
+<?php
 echo Consultation::afficherNbHeuresParMedecin();
 ?>
